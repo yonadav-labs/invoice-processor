a4f8cd39e2a2f11b6c93c8375ee448edd1664b69
yonadav-labs <yonadav.barilan.upwork@gmail.com>
2020-10-28 09:51:52 -0700

    use env for emails

--- sqs-poll.py
+++ sqs-poll.py
@@ -1,3 +1,5 @@
+import os
+
 import invoice_process
 
 from utilities import send_email, get_sqs_resource
@@ -9,8 +11,8 @@ MAX_QUEUE_MESSAGES = 1
 sqs = get_sqs_resource()
 queue = sqs.get_queue_by_name(QueueName=QUEUE_NAME)
 
-from_email = 'LTC <it.corridor051@gmail.com>'
-to_emails = 'jason.5001001@gmail.com'
+from_email = os.getenv("FROM_EMAIL", 'LTC <it.corridor051@gmail.com>')
+to_email = os.getenv("TO_EMAIL", 'jason.5001001@gmail.com')
 
 TEST_MODE = True
 
@@ -23,9 +25,9 @@ while True:
         result, log_file, invoice_info = invoice_process.validate_file(file_name, TEST_MODE)
         if result:
             result = invoice_process.process_invoice(invoice_info, log_file, TEST_MODE)
-            email_body = 'Inserted successfully' if result else 'Insertion failed'
+            email_body = 'Uploaded successfully' if result else 'Insertion failed'
         else:
             email_body = 'Validation failed'
 
-        subject = f'Invoice Processed ({file_name})'
-        send_email(subject, from_email, to_emails, email_body, log_file)
+        subject = f'Invoice Uploaded Successfully ({file_name})'
+        send_email(subject, from_email, [to_email], email_body, log_file)
